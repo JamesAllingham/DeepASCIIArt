14d44b1d8acb24e84dfcc064d3646a187579f76d
James Allingham <james.allingham@gmail.com>
2017-05-14 11:36:10 +0200

    Added code to crop the input images to be a consistent size
    Also changed the way the file was written by jp2a

--- generate_data.py
+++ generate_data.py
@@ -1,14 +1,25 @@
-import glob
+from glob import glob
 from subprocess import call
+from scipy.misc import imread, imsave
+import numpy as np
 
 def main():
     data_dir = "Data/"
 
     # get a list of all the JPEG images in the Data dir
-    input_files = glob.glob(data_dir + "*jpg")
+    input_files = glob(data_dir + "*.JPEG")
+
+    # for each JPEG crop the image to get a square
+    for i, fname in enumerate(input_files):
+        img = np.float32(imread(fname, mode='RGB'))
+        img = img[:300, :300, :]
+        imsave(data_dir + "%s.jpg" % i, img)
 
     # for each JPEG run the jp2a utility to convert it to ASCII pic with width = height = 100
-    [call(['jp2a', input_file, "--width=100", "--grayscale", "--output=" + data_dir + "%s.ascii" % i]) for i, input_file in enumerate(input_files)]
+    for i, input_file in enumerate(input_files):
+        with open(data_dir + "%s.ascii" % i, 'w') as ofile:
+            call(['jp2a', input_file, "--size=150x150", "--background=light"], stdout=ofile)
+
 
 if __name__ == "__main__":
     main()
