099c0443a9f3448dd99496be1f5f08862e2e019f
James Allingham <james.allingham@gmail.com>
2017-06-17 13:35:25 +0200

    Fixed a bug with generating ASCII art from original (non-cropped) images
    Also added some command line args
    
    Also added some checks for exisiting dirs

--- generate_data.py
+++ generate_data.py
@@ -1,11 +1,20 @@
 from glob import glob
 from subprocess import call
+from random import random, seed
+import os
+import argparse
 from scipy.misc import imread, imsave
 import numpy as np
-from random import random, seed
 
-def main():
-    data_dir = "Data/"
+def main(data_dir, size):
+    
+    if not os.path.exists(data_dir):
+        print("data_dir {} does not exist.".format(data_dir))
+        exit(1)
+
+    for direc in [data_dir + "test/", data_dir + "train/", data_dir + "valid/"]:
+        if not os.path.exists(direc):
+            os.makedirs(direc)    
 
     # get a list of all the JPEG images in the Data dir
     input_files = glob(data_dir + "*.JPEG")
@@ -15,17 +24,28 @@ def main():
     split = ["test/" if random() <= 0.2 else "train/" if random() <= 0.8 else "valid/" for _ in range(0, len(input_files))]
 
     # for each JPEG crop the image to get a square
+    cropped_files = []
     for i, fname in enumerate(input_files):
         img = np.float32(imread(fname, mode='RGB'))
-        img = img[:300, :300, :]
+        img = img[:size, :size, :]
         imsave(data_dir + split[i] + "%s.jpg" % i, img)
+        cropped_files.append("%s.jpg" % i)
 
-    # for each JPEG run the jp2a utility to convert it to ASCII pic with width = height = 100
-    for i, input_file in enumerate(input_files):
+    # for each cropped image run the jp2a utility to convert it to ASCII pic with width = height = 100
+    for i, input_file in enumerate(cropped_files):
         with open(data_dir + split[i] + "%s.ascii" % i, 'w') as ofile:
             call(['jp2a', input_file, "--size=150x75", "--background=light"], stdout=ofile)
 
 
 if __name__ == "__main__":
-    # TO DO: make the source destination for the data a commandline arg
-    main()
+    parser = argparse.ArgumentParser(description='Process JPEG files in data_dir resizing to a constant size and creating ASCII art.')
+    parser.add_argument('--data_dir', metavar='D', default="Data/", 
+                        help="""Specify the directory where the unprocessed files can be found. 
+                        The output files will be written to data_dir/test, data_dir/train, and data_dir/valid. 
+                        The default is 'Data/'""")
+    parser.add_argument('--size', metavar='S', default=300, 
+                        help="""Specify the dimensions of the SxS output images after resizing. 
+                        The default is 300x300.""")
+    args = parser.parse_args()
+    main(args.data_dir, args.size)
+    
